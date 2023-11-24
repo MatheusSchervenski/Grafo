c2487cdefe15548699012dcd561d1536f92d3b4b
MatheusSchervenski <matheuslsvilela@academico.unirv.edu.br>
2023-11-24 19:52:45 -0300

    Descrição das alterações

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pessoas, setPessoas] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/pessoas')
+      .then((response) => setPessoas(response.data))
+      .catch((error) => console.error('Erro ao buscar pessoas:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Rede Social</h1>
+      <ul>
+        {pessoas.map((pessoa) => (
+          <li key={pessoa.id}>{pessoa.nome} (ID: {pessoa.id})</li>
+        ))}
+      </ul>
     </div>
   );
 }
